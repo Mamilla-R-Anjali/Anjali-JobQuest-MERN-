0c3bb8d7940440570bf4323771b01d8d1ffc303a
Mamilla-R-Anjali <mamillaanjali8@gmail.com>
2026-07-29 17:42:44 +0530

    fix: allow Netlify frontend through CORS

--- SERVER/index.ts
+++ SERVER/index.ts
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://anjali-jobquest.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
