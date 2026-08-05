009ba42c53112dfe959787a7142cf99193723ab9
Mamilla-R-Anjali <mamillaanjali8@gmail.com>
2026-08-05 16:25:12 +0530

    Connect frontend to Railway backend

--- CLIENT/src/axiosapi.jsx
+++ CLIENT/src/axiosapi.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
-export const baseURL = "https://anjali-jobquest-mern.onrender.com";
+
+export const baseURL =
+  "https://anjali-jobquest-mern-production.up.railway.app";
+
 const axiosapi = axios.create({
   baseURL,
 });
+
 export default axiosapi;
