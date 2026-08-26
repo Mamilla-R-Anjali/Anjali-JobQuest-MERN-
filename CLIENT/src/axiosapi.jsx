e1448ca9bd5bb21fb27715e73c839cf8d1e8a6b5
Mamilla-R-Anjali <133332624+Mamilla-R-Anjali@users.noreply.github.com>
2026-08-26 13:13:02 +0530

    Update backend URL to Render

--- CLIENT/src/axiosapi.jsx
+++ CLIENT/src/axiosapi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseURL =
-  "https://anjali-jobquest-mern-production.up.railway.app";
+  "https://anjali-jobquest-mern-l8h7.onrender.com";
 
 const axiosapi = axios.create({
   baseURL,
