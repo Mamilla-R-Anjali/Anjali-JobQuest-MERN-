6deee160419fb6170e4a9677c197904bd6c2152a
Mamilla-R-Anjali <133332624+Mamilla-R-Anjali@users.noreply.github.com>
2026-09-03 14:21:55 +0530

    Delete backend directory

--- backend/src/main/java/com/interview/platform/repository/InterviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.interview.platform.repository;
-
-import com.interview.platform.model.Interview;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterviewRepository extends JpaRepository<Interview, Long> {
-}
