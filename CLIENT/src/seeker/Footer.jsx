0a610e3dfe759012d87e97d2cc1e62addfc17cfd
Mamilla-R-Anjali <mamillaanjali8@gmail.com>
2026-09-03 14:13:32 +0530

    Updated copyright to 2026

--- CLIENT/src/seeker/Footer.jsx
+++ CLIENT/src/seeker/Footer.jsx
@@ -6,11 +6,7 @@ const Footer = () => {
       <div className="bg-blue  mt-xl-5 " style={{ overflowX: "hidden" }}>
         <div className="row px-3 text-center ">
           <small className="ml-4 ml-sm-5 mb-2">
-<<<<<<< HEAD
             Copyright © 2026. All rights reserved.
-=======
-            Copyright © 2024. All rights reserved.
->>>>>>> 1383e3f2c07bc79bc040ee887f1e9fa827ec0919
           </small>
           <div className="social-contact ml-4 ml-sm-auto">
             <span className="fa fa-facebook mr-4 text-sm ms-2 " />
